c29d9e34bc4ec6b27cb204b535382f08956dc1f5
Burcu Bayraö <burju7@yahoo.com>
2023-06-21 15:03:26 +0300

    overall done

--- app/memes/page.js
+++ app/memes/page.js
@@ -2,8 +2,12 @@
 
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 const Memes = () => {
+  const [memeIndex, setMemeIndex] = useState(0);
+  const [captions, setCaptions] = useState([]);
+
   const queryClient = useQueryClient();
 
   const { isLoading, error, data } = useQuery({
@@ -13,13 +17,25 @@ const Memes = () => {
         res.json().then((res.json = data))
       ),
   });
+  const memes = data.data.memes;
+
+  React.useEffect(() => {
+    if (memes.length) {
+      setCaptions(Array(memes[memeIndex].box_count).fill(""));
+    }
+  }, [memeIndex, memes]);
+
+  useEffect(() => {
+    console.log(captions);
+  }, [captions]);
 
   if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
 
-  const memes = data.data.memes;
-  console.log(memes);
+  // console.log(memes);
+
+  // console.log(memes);
 
   //   const mutation = useMutation({
   //     mutationFn: postMemes,
@@ -29,15 +45,49 @@ const Memes = () => {
   //     },
   //   });
 
+  const randomImageNumber = Math.floor(Math.random(memes) * memes.length);
+  const randomImage = memes[randomImageNumber];
+  // console.log(randomImage);
+
   return (
-    <div>
-      {memes.map((meme) => (
-        <>
-          <div style={{}}>
-            <p key={meme.id}>{meme.name}</p>
-          </div>
-        </>
-      ))}
+    <div className="h-screen">
+      <div className=" ">
+        <h1 className="text-center text-lg font-semibold tracking-wide mb-5">
+          Meme Generator
+        </h1>
+        <div className="container w-10/12 mx-auto flex flex-grid items-center justify-center py-5 gap-5">
+          <button
+            className="border border-gray-700 border-2 bg-gray-300 rounded-2xl p-2"
+            onClick={() => console.log("Generate")}
+          >
+            Generate
+          </button>
+          <button
+            className="border border-gray-700 border-2 rounded-2xl p-2"
+            onClick={() => setMemeIndex(memeIndex + 1)}
+          >
+            Skip
+          </button>
+        </div>
+      </div>
+      <div className="container w-10/12 mx-auto flex flex-grid items-center justify-center py-5 gap-5">
+        <div className="relative rounded ">
+          <img
+            src={randomImage.url}
+            width={1200}
+            height={900}
+            className="w-full aspect-square "
+            alt={randomImage.name}
+          />
+        </div>
+        <div>
+          {
+            captions.map((caption, index)=>(
+              <input key={index} type="text" className=" bg-gradient-to-r from-gray-100 to-gray-300 p-2 rounded-3xl font-medium" placeholder="Write a caption"   />
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 };
