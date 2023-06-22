a3efaee5eff3edc80e9f1e85eab7b9a6af33473e
Burcu Bayraö <burju7@yahoo.com>
2023-06-23 01:08:28 +0300

    added generated meme features

--- app/memes/page.js
+++ app/memes/page.js
@@ -1,91 +1,136 @@
 "use client";
-
+import Script from "next/script";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Modal from "app/modal.js";
+import "flowbite";
 
-const Memes = () => {
+const Memes = (defaultModal) => {
+  const [memes, setMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
   const [captions, setCaptions] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+  const [generated, setGenerated] = useState();
 
-  const queryClient = useQueryClient();
+  const router = useRouter();
+  // const queryClient = useQueryClient();
 
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["repoData"],
-    queryFn: () =>
-      fetch("https://api.imgflip.com/get_memes").then((res) =>
-        res.json().then((res.json = data))
-      ),
-  });
-  const memes = data.data.memes;
+  const updateCaption = (e, index) => {
+    const text = e.target.value || "";
+    setCaptions(
+      captions.map((c, i) => {
+        if (index === i) {
+          return text;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
 
-  React.useEffect(() => {
+  const generateMeme = () => {
+    const currentMeme = memes[memeIndex];
+    const formData = new FormData();
+    formData.append("username", "BurcuYilmaz");
+    formData.append("password", "2522529b");
+    formData.append("template_id", currentMeme.id);
+    captions.forEach((caption, index) =>
+      formData.append(`boxes[${index}][text]`, caption)
+    );
+    fetch("https://api.imgflip.com/caption_image", {
+      method: "POST",
+      body: formData,
+    }).then((res) =>
+      res.json().then((res) => {
+        console.log(res.data.url);
+        setGenerated(res.data.url);
+      })
+    );
+    setIsShown(true);
+
+    console.log(isShown);
+  };
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((res) => {
+        const _memes = res.data.memes;
+        shuffleArray(_memes);
+        setMemes(_memes);
+      });
+  }, []);
+
+  useEffect(() => {
     if (memes.length) {
       setCaptions(Array(memes[memeIndex].box_count).fill(""));
     }
   }, [memeIndex, memes]);
-
-  useEffect(() => {
-    console.log(captions);
-  }, [captions]);
-
-  if (isLoading) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
-  // console.log(memes);
-
-  // console.log(memes);
-
-  //   const mutation = useMutation({
-  //     mutationFn: postMemes,
-  //     onSuccess: () => {
-  //       // Invalidate and refetch
-  //       queryClient.invalidateQueries({ queryKey: ["memes"] });
-  //     },
-  //   });
-
-  const randomImageNumber = Math.floor(Math.random(memes) * memes.length);
-  const randomImage = memes[randomImageNumber];
-  // console.log(randomImage);
-
+  console.log(isShown);
   return (
-    <div className="h-screen">
-      <div className=" ">
-        <h1 className="text-center text-lg font-semibold tracking-wide mb-5">
-          Meme Generator
-        </h1>
-        <div className="container w-10/12 mx-auto flex flex-grid items-center justify-center py-5 gap-5">
-          <button
-            className="border border-gray-700 border-2 bg-gray-300 rounded-2xl p-2"
-            onClick={() => console.log("Generate")}
-          >
-            Generate
-          </button>
-          <button
-            className="border border-gray-700 border-2 rounded-2xl p-2"
-            onClick={() => setMemeIndex(memeIndex + 1)}
-          >
-            Skip
-          </button>
-        </div>
+    <div className="h-screen w-full p-5 bg-gradient-to-r from-blue-200 to-transparent">
+      {isShown && generated && (
+        <Modal generated={generated} setGenerated={setGenerated} />
+      )}
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.js"></Script>
+      <h1 className="text-center text-2xl font-semibold tracking-wide mb-2 ">
+        Meme Generator
+      </h1>
+      <p className="text-center text-xl">
+        The Fastest Meme Generator on the Planet. Easily add text to images or
+        memes.
+      </p>
+
+      <div className="flex flex-grid justify-center py-5 gap-5 ">
+        <button
+          className="border border-gray-700 border-2 bg-gray-300 rounded-2xl p-2"
+          onClick={generateMeme}
+        >
+          Generate
+        </button>
+        <button
+          className="border border-gray-700 border-2 rounded-2xl p-2"
+          onClick={() => setMemeIndex(memeIndex + 1)}
+        >
+          Skip
+        </button>
       </div>
-      <div className="container w-10/12 mx-auto flex flex-grid items-center justify-center py-5 gap-5">
-        <div className="relative rounded ">
-          <img
-            src={randomImage.url}
-            width={1200}
-            height={900}
-            className="w-full aspect-square "
-            alt={randomImage.name}
-          />
-        </div>
-        <div>
-          {
-            captions.map((caption, index)=>(
-              <input key={index} type="text" className=" bg-gradient-to-r from-gray-100 to-gray-300 p-2 rounded-3xl font-medium" placeholder="Write a caption"   />
-            ))
-          }
+
+      <div id="left" className="grid grid-cols-2 gap-4  ">
+        {memes.length ? (
+          <div className="shadow-xl rounded ">
+            <img
+              src={memes[memeIndex].url}
+              width={memes[memeIndex].width}
+              height={memes[memeIndex].height}
+              alt={memes.name}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+
+        <div id="right" className="flex flex-col m-2">
+          {captions.map((c, index) => (
+            <input
+              key={index}
+              type="text"
+              className=" bg-gradient-to-r from-gray-100 to-gray-300 p-2 m-2 rounded-3xl font-small"
+              placeholder="Write your caption"
+              onChange={(e) => updateCaption(e, index)}
+            />
+          ))}
         </div>
       </div>
     </div>
